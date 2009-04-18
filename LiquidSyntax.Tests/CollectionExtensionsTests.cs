c567bae658b9cd9fa992c045838f52ce6c0c900c
matthew.a.frank <matthew.a.frank@13bc36a4-2645-11de-ab75-c7642394e8b2>
2009-04-18 14:37:12 +0000

    added IEnumerable.IsEmpty and .IsNotEmpty

--- LiquidSyntax.Tests/CollectionExtensionsTests.cs
+++ LiquidSyntax.Tests/CollectionExtensionsTests.cs
@@ -22,5 +22,17 @@ namespace LiquidSyntax.Tests {
             list.Should(Be.EqualTo(new[]{instance}));
             list.Should(Be.InstanceOfType(typeof(List<FakeDisposable>)));
         }
+
+        [Test]
+        public void ShouldIdentifyEmptyCollection() {
+            new int[] {}.IsEmpty().Should(Be.True);
+            new[] {1}.IsEmpty().Should(Be.False);
+        }
+
+        [Test]
+        public void ShouldIdentifyNonEmptyCollection() {
+            new int[] {}.IsNotEmpty().Should(Be.False);
+            new[] {1}.IsNotEmpty().Should(Be.True);
+        }
     }
 }
