1272b0aadfc250c200654a39c5e6b8264ec653b6
matthew.a.frank <matthew.a.frank@13bc36a4-2645-11de-ab75-c7642394e8b2>
2009-07-07 17:34:24 +0000

    added string.Reversed()

--- trunk/LiquidSyntax/StringExtensions.cs
+++ trunk/LiquidSyntax/StringExtensions.cs
@@ -1,7 +1,14 @@
+using System.Linq;
+
 namespace LiquidSyntax {
     public static class StringExtensions {
         public static string Substitute(this string format, params object[] args) {
             return string.Format(format, args);
+        }
+
+        public static string Reversed(this string original) {
+            if (original == null) return null;
+            return original.Reverse().Join(string.Empty);
         }
     }
 }
