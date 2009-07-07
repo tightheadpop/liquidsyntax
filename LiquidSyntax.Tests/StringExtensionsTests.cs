9ab70b9075618da1ead526838bce435f2fd019e2
matthew.a.frank <matthew.a.frank@13bc36a4-2645-11de-ab75-c7642394e8b2>
2009-07-07 17:34:24 +0000

    added string.Reversed()

--- LiquidSyntax.Tests/StringExtensionsTests.cs
+++ LiquidSyntax.Tests/StringExtensionsTests.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using NUnit.Framework;
 using LiquidSyntax.ForTesting;
+using System.Linq;
 
 namespace LiquidSyntax.Tests {
     [TestFixture]
@@ -8,6 +9,17 @@ namespace LiquidSyntax.Tests {
         [Test]
         public void ShouldSubstitutePlaceholders() {
             "{0}-{2}-1-{1}".Substitute(new StringBuilder("hi"), 2, "peanut").Should(Be.EqualTo("hi-peanut-1-2"));
+        }
+
+        [Test]
+        public void CanReverseString() {
+            "123".Reversed().Should(Be.EqualTo("321"));
+            "".Reversed().Should(Be.EqualTo(""));
+        }
+
+        [Test]
+        public void DoesNotAttemptToReverseNullString() {
+            ((string)null).Reversed().Should(Be.Null);
         }
     }
 }
