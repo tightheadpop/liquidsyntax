c0999d2f996ef348dd28677b4d6272b0037102e4
mikehale <mikehale@13bc36a4-2645-11de-ab75-c7642394e8b2>
2009-10-26 21:27:09 +0000

    Added ToSet and AsSet CollectionExtension methods

--- LiquidSyntax.Tests/CollectionExtensionsTests.cs
+++ LiquidSyntax.Tests/CollectionExtensionsTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using LiquidSyntax.ForTesting;
 using NUnit.Framework;
@@ -7,13 +6,10 @@ namespace LiquidSyntax.Tests {
     [TestFixture]
     public class CollectionExtensionsTests {
         [Test]
-        public void ShouldSortNaturally() {
-            new List<int> {3, 4, 2}.Sorted().Should(Be.EqualTo(new[] {2, 3, 4}));
-        }
-
-        [Test]
-        public void ShouldJoinWithDelimiter() {
-            new List<int> {3, 4, 2}.Join(", ").Should(Be.EqualTo("3, 4, 2"));
+        public void ForEachShouldEnumerateItemsAndPerformAction() {
+            var total = 0;
+            new[] {1, 2, 3}.ForEach(i => total += i);
+            total.Should(Be.EqualTo(6));
         }
 
         [Test]
@@ -25,22 +21,11 @@ namespace LiquidSyntax.Tests {
         }
 
         [Test]
-        public void ShouldIdentifyEmptyCollection() {
-            new int[] {}.IsEmpty().Should(Be.True);
-            new[] {1}.IsEmpty().Should(Be.False);
-        }
-
-        [Test]
-        public void ShouldIdentifyNonEmptyCollection() {
-            new int[] {}.IsNotEmpty().Should(Be.False);
-            new[] {1}.IsNotEmpty().Should(Be.True);
-        }
-
-        [Test]
-        public void ForEachShouldEnumerateItemsAndPerformAction() {
-            var total = 0;
-            new[] {1, 2, 3}.ForEach(i => total += i);
-            total.Should(Be.EqualTo(6));
+        public void ShouldCreateSetFromSingleObject() {
+            var instance = new FakeDisposable();
+            var set = instance.AsSet();
+            set.Should(Be.EqualTo(new[] {instance}));
+            set.Should(Be.InstanceOf(typeof(HashSet<FakeDisposable>)));
         }
 
         [Test]
@@ -56,15 +41,45 @@ namespace LiquidSyntax.Tests {
         }
 
         [Test]
-        public void ShouldSortEnumerableByComparisonMethod() {
-            var ints = new[] {3,2,6};
-            ints.SortedBy((a,b) => a-b).Should(Be.EqualTo(new[]{2,3,6}));
+        public void ShouldIdentifyEmptyCollection() {
+            new int[] {}.IsEmpty().Should(Be.True);
+            new[] {1}.IsEmpty().Should(Be.False);
+        }
+
+        [Test]
+        public void ShouldIdentifyNonEmptyCollection() {
+            new int[] {}.IsNotEmpty().Should(Be.False);
+            new[] {1}.IsNotEmpty().Should(Be.True);
+        }
+
+        [Test]
+        public void ShouldJoinWithDelimiter() {
+            new List<int> {3, 4, 2}.Join(", ").Should(Be.EqualTo("3, 4, 2"));
         }
 
         [Test]
         public void ShouldSortEnumerableByComparer() {
-            var ints = new[] { 3, 2, 6 };
-            ints.SortedBy(new IntegerComparer()).Should(Be.EqualTo(new[] { 2, 3, 6 }));
+            var ints = new[] {3, 2, 6};
+            ints.SortedBy(new IntegerComparer()).Should(Be.EqualTo(new[] {2, 3, 6}));
+        }
+
+        [Test]
+        public void ShouldSortEnumerableByComparisonMethod() {
+            var ints = new[] {3, 2, 6};
+            ints.SortedBy((a, b) => a - b).Should(Be.EqualTo(new[] {2, 3, 6}));
+        }
+
+        [Test]
+        public void ShouldSortNaturally() {
+            new List<int> {3, 4, 2}.Sorted().Should(Be.EqualTo(new[] {2, 3, 4}));
+        }
+
+        [Test]
+        public void ToSetShouldRemoveDuplicates() {
+            var list = new List<string> {"a", "b", "a"};
+            var set = list.ToSet();
+            set.Should(Be.EqualTo(new[] {"a", "b"}));
+            set.Should(Be.InstanceOf(typeof(HashSet<string>)));
         }
 
         private class IntegerComparer : Comparer<int> {
